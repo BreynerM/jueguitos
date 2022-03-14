0cdc4272b591905ff3fe5f784d925743026bb6ea
Taller 1-202 <Estudiante1202@talleresinpahu.edu>
2022-03-14 11:03:28 -0500

    actualizacion index

--- index.py
+++ index.py
@@ -1,60 +1,50 @@
-import pygame, sys, random
-from enemy import * 
+from turtle import Screen
+import pygame,sys
 
-
-size  = (800, 500)
-
-lista_meter = []
-
-RED = (255, 51, 81)
-WHITE = (250, 250, 250)
-BLACK = (10, 10, 10)
-
-
-
-
-for i in range (30):
-    size = random.randint(10, 20)
-    velocidad_x = random.uniform(-1 ,1)
-    velocidad_y = random.randint(1,3)
-    position_x = random.randint(100,900)
-    position_y = random.randint(0,100)
-    color = RED
-    meter = meterorito(size, velocidad_x, velocidad_y,color, position_x, position_y)
-    lista_meter.append(meter)
-    
-    
-
+BLACK = (0, 0, 0)
+WHITE = (225, 225, 225)
+size = (800, 500)
+pygame.init()
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((1000, 600))
+pygame.mixer.init()
+
+coord_x = 350
+coord_y = 425 
+
+x_speed = 0
+y_speed = 0
 
 
-while True:
+#Cerrar ventana 
+while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
-    
-    screen.fill(WHITE)
-
-    for meter in lista_meter:
-        pygame.draw.circle(screen,meter.color,(meter.position_x, meter.position_y), meter.size)  
-        meter.position_x += meter.velocidad_x
-        meter.position_y += meter.velocidad_y
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+            if event.key == pygame.K_RIGHT:
+                x_speed = 3
         
-        if meter.position_y > 620:
-            meter.position_y = 0
-            meter.velocidad_y = random.randint(1 , 4)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                x_speed = -10
+            if event.key == pygame.K_RIGHT:
+                x_speed = 10
 
-        if meter.position_x > 1000 or position_x < 0:
-            meter.position_x = random.randint(100,900)
+        screen.fill(WHITE)
+
+        coord_x += x_speed
+
+        pygame.draw.rect(screen, BLACK, (coord_x, coord_y, 50,50))
+        
+
+        pygame.display.flip()
 
 
 
-
-    pygame.display.flip()
-    clock.tick(30)
-    
 
   
 
